8998652caa19974ad660e1588b6f661e3f506283
Linus <lyan2@ebay.com>
2017-07-25 22:34:43 +0800

    remove iaf dependencies

--- src/main/java/com/linus/security/spring3/controllers/MainController.java
+++ src/main/java/com/linus/security/spring3/controllers/MainController.java
@@ -1,16 +1,17 @@
 package com.linus.security.spring3.controllers;
 
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.linus.security.spring3.Router;
-import com.sun.istack.internal.logging.Logger;
 
 @Controller
 public class MainController {
-	Logger logger = Logger.getLogger(MainController.class);
+	private static final Logger logger = Logger.getLogger(MainController.class.getName());
 
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView handleRequest() {
